faef1ffbda94af0ee7ac667bb026c53bca6df2ae
Nam Pham Ngoc Quang <nampnq@gmail.com>
2013-01-28 04:36:15 +0700

    Change setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
  
-long_description = open('README.txt').read()
+#long_description = open('README.txt').read()
  
 setup(
     name='django-tagging-autocomplete',
     version='0.3.1',
     description='Autocompletion for django-tagging',
-    long_description=long_description,
+    long_description='Autocompletion for django-tagging',
     author='Ludwik Trammer',
     author_email='ludwik@gmail.com',
     url='http://code.google.com/p/django-tagging-autocomplete/',
